4e6608a1fded1a93f2f433417ecebc52d3e0f2ad
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-03 19:14:31 -0800

    finished body copy thanks Sarah

--- src/intro.js
+++ src/intro.js
@@ -1,10 +1,9 @@
 $(function(){
       $(".element").typed({
-          strings: [ "Hi, I'm Nico. <br> I design and develop web applications. <br> Some of my clients include: <br> ^1000 Dare, <br> ^1000 SAP, <br> ^1000 and Corvere Ads.",
-           "If you are inquiring about freelance work <br> or interested in hiring me full-time. <br> Shoot me good ole' fashioned email. <br> Thanks."],
+          strings: [ "Hi, I'm Nico. <br> I design and develop web applications. <br> Some of my clients include: <br> ^1000 Dare, ^1000 SAP ^1000 and Corvere Ads.",
+           "If you are inquiring about hiring me for <br> freelance or full-time work, <br> shoot me good ole' fashioned email. <br> Thanks."],
           typeSpeed: 10,
           startDelay: 8800,
           showCursor: false
       });
   });
-
