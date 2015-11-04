80e46ce8a9e5d7b74cc9c9851d9251dcbf5186b4
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-04 14:04:21 -0800

    adds finished menu setTimeout MEDIA QUERY TIME

--- src/intro.js
+++ src/intro.js
@@ -1,9 +1,10 @@
 $(function(){
-      $(".element").typed({
-          strings: [ "Hi, I'm Nico. <br> I design and develop web applications. <br> I've worked with: <br> ^1000 Dare, ^1000 SAP ^1000 and Corvere Ads.",
-           "^1500 If you are thinking about hiring me for <br> full-time or for freelance work, <br> shoot me good ole' fashioned email. <br><br> Thanks."],
-          typeSpeed: 10,
-          startDelay: 8800,
-          showCursor: false
-      });
+  $(".element").typed({
+      strings: [ "Hi, I'm Nico. <br> I design and develop web applications. <br> I'm passionate about creating beautiful User Interfaces <br> with meticulously crafted User Experiences.",
+       "To hire me for either full-time or freelance work, shoot me an ole' fashioned email. <br><br> Thanks."],
+      typeSpeed: 10,
+      startDelay: 8800,
+      showCursor: false,
+      backDelay: 1800
   });
+});
