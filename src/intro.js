2890a78ddf818b66ecc146973a7917cc20f9ed84
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-03 19:27:15 -0800

    final body copy need to add email button now

--- src/intro.js
+++ src/intro.js
@@ -1,7 +1,7 @@
 $(function(){
       $(".element").typed({
-          strings: [ "Hi, I'm Nico. <br> I design and develop web applications. <br> Some of my clients include: <br> ^1000 Dare, ^1000 SAP ^1000 and Corvere Ads.",
-           "If you are inquiring about hiring me for <br> freelance or full-time work, <br> shoot me good ole' fashioned email. <br> Thanks."],
+          strings: [ "Hi, I'm Nico. <br> I design and develop web applications. <br> I've worked with: <br> ^1000 Dare, ^1000 SAP ^1000 and Corvere Ads.",
+           "If you are thinking about hiring me for <br> full-time or for freelance work, <br> shoot me good ole' fashioned email. <br><br> Thanks."],
           typeSpeed: 10,
           startDelay: 8800,
           showCursor: false
