832852962565a6aa68fa7c330618bec7c0a35ec5
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-03 19:30:10 -0800

    body copy done

--- src/intro.js
+++ src/intro.js
@@ -1,7 +1,7 @@
 $(function(){
       $(".element").typed({
           strings: [ "Hi, I'm Nico. <br> I design and develop web applications. <br> I've worked with: <br> ^1000 Dare, ^1000 SAP ^1000 and Corvere Ads.",
-           "If you are thinking about hiring me for <br> full-time or for freelance work, <br> shoot me good ole' fashioned email. <br><br> Thanks."],
+           "^1500 If you are thinking about hiring me for <br> full-time or for freelance work, <br> shoot me good ole' fashioned email. <br><br> Thanks."],
           typeSpeed: 10,
           startDelay: 8800,
           showCursor: false
