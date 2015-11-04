7bbaf07389e54f6b1ae1bfdf4b67fc988697e46f
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-03 17:04:42 -0800

    adds email and first paragraph

--- src/intro.js
+++ src/intro.js
@@ -1,8 +1,9 @@
- $(function(){
-        $(".element").typed({
-            strings: [ "Hi, I'm Nico. <br> I'm a User Interface Designer and Developer based in Vancouver. <br> Welcome "],
-            typeSpeed: .01,
-            startDelay: 8800,
-            showCursor: false
-        });
-    });
+$(function(){
+      $(".element").typed({
+          strings: [ "Hi, I'm Nico. <br> ^1000 I Design and Develop Web Applications. <br> ^1000 If you are inquiring about freelance work <br> ^1000 or interested in hiring me full-time. <br> ^1000 Send me an email at nicoflojo.developer@gmail.com"],
+          typeSpeed: 2,
+          startDelay: 8800,
+          showCursor: false
+      });
+  });
+
