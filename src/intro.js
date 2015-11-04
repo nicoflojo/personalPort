d80ad4347a0ed23b27851b5fe3e7d839fdda1ab2
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-03 16:28:10 -0800

    first commit

---
+++ src/intro.js
@@ -0,0 +1,8 @@
+ $(function(){
+        $(".element").typed({
+            strings: [ "Hi, I'm Nico. <br> I'm a User Interface Designer and Developer based in Vancouver. <br> Welcome "],
+            typeSpeed: .01,
+            startDelay: 8800,
+            showCursor: false
+        });
+    });
