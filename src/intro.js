90fe26d83cf32508ba5fd0741a71498cbdd78e7d
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-05 13:43:17 -0800

    changed typespeed and backspeed FINAL

--- src/intro.js
+++ src/intro.js
@@ -1,10 +1,12 @@
 $(function(){
   $(".element").typed({
-      strings: [ "Hi, I'm Nico. <br> I design and develop web applications. <br> I'm passionate about creating beautiful User Interfaces <br> with meticulously crafted User Experiences.",
-       "To hire me for either full-time or freelance work, shoot me an ole' fashioned email. <br><br> Thanks."],
-      typeSpeed: 10,
+      strings: [
+      "Hi, I'm Nico. <br> I design and develop web applications. <br> I'm passionate about creating beautiful User Interfaces <br> with meticulously crafted User Experiences.",
+      "I've worked with: <br> DARE, <br> SAP, <br> Blast Radius <br> and now I'm on the Voxiter team.",
+       "^2000 To hire me for either full-time or freelance work, shoot me an ole' fashioned email. <br><br> Thanks."],
+      typeSpeed: 8,
       startDelay: 8800,
       showCursor: false,
-      backDelay: 1800
+      backSpeed: -9999,
   });
 });
