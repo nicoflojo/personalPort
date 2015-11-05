f9b7533bd24ea7b70aaf2b60c18f4c9451b57f28
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-05 13:57:07 -0800

    added corvere removed mintyped

--- src/intro.js
+++ src/intro.js
@@ -2,7 +2,7 @@ $(function(){
   $(".element").typed({
       strings: [
       "Hi, I'm Nico. <br> I design and develop web applications. <br> I'm passionate about creating beautiful User Interfaces <br> with meticulously crafted User Experiences.",
-      "I've worked with: <br> DARE, <br> SAP, <br> Blast Radius <br> and now I'm on the Voxiter team.",
+      "I've worked with: <br> DARE, <br> SAP, <br> Corvere Ads <br> and now I'm on the Voxiter team.",
        "^1500 To hire me for either full-time or freelance work, shoot me an ole' fashioned email. <br><br> Thanks."],
       typeSpeed: 8,
       startDelay: 8800,
