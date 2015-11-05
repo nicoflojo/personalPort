c53db18f19f902ed54e35f49c00db9c4062cf47f
nico.flojo <nicoflojo.developer@gmail.com>
2015-11-05 13:55:47 -0800

    minified most files

--- libs/jquery-loader.js
+++ libs/jquery-loader.js
@@ -1,12 +1,1 @@
-!(function() {
-
-  // Get any lib=___ param from the query string.
-  var library = location.search.match(/[?&]lib=(.*?)(?=&|$)/);
-
-  /* jshint -W060 */
-  if (library) {
-    document.write('<script src="../libs/' + library[1] + '"></script>');
-  } else {
-    document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>');
-  }
-}());
+!function(){var c=location.search.match(/[?&]lib=(.*?)(?=&|$)/);c?document.write('<script src="../libs/'+c[1]+'"></script>'):document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>')}();
--- src/intro.js
+++ src/intro.js
@@ -3,7 +3,7 @@ $(function(){
       strings: [
       "Hi, I'm Nico. <br> I design and develop web applications. <br> I'm passionate about creating beautiful User Interfaces <br> with meticulously crafted User Experiences.",
       "I've worked with: <br> DARE, <br> SAP, <br> Blast Radius <br> and now I'm on the Voxiter team.",
-       "^2000 To hire me for either full-time or freelance work, shoot me an ole' fashioned email. <br><br> Thanks."],
+       "^1500 To hire me for either full-time or freelance work, shoot me an ole' fashioned email. <br><br> Thanks."],
       typeSpeed: 8,
       startDelay: 8800,
       showCursor: false,
